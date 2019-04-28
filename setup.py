170c352ab923d3799b9c1cb873b4896a881a42f9
Diego Oliveira Rodrigues <diegopso2@gmail.com>
2019-04-28 14:56:14 +0000

    adding new plots and more config

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ config = {
     'name': "Minicourse SBRC'2019",
     'version': '0.2',
     'description': 'Exploring hybrid multi-modal urban routes collected from tweets in São Paulo.',
-    'author': 'Diego Oliveira',
+    'author': 'Diego Oliveira and Frances Santos',
     'url': '--',
     'download_url': '--',
-    'author_email': 'diego@lrc.ic.unicamp.br',
-    'install_requires': ['pandas', 'haversine', 'numpy', 'scipy', 'matplotlib', 'sklearn', 'hdbscan', 'xmltodict']
+    'author_email': '[diego, francessantos]@lrc.ic.unicamp.br',
+    'install_requires': ['pandas', 'haversine', 'numpy', 'scipy', 'matplotlib', 'sklearn', 'hdbscan', 'xmltodict', 'beautifulsoup4', 'googlemaps', 'gmplot', 'seaborn', 'json']
 }
 
 setup(**config)
